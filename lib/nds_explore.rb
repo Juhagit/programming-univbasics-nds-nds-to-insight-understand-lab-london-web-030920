d978f5927402663ef082ea03076b37d24cbb16f8
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-27 04:32:24 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -14,4 +14,10 @@ pp directors_database
 def print_first_directors_movie_titles
   spiel = directors_database[0][:movies]
   spiel_mov = 0
- 
+  while spiel_mov < spiel.count do
+    puts spiel[spiel_mov][:title]
+    spiel_mov += 1 
+  end 
+end
+
+
